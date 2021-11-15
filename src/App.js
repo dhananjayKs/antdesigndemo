7734c4966e621cc3bf4d5ef134a4aef737499e54
Dhananjaya K S <ksdhananjaya05@gmail.com>
2021-11-15 17:02:36 +0530

    customizato=io of the def features

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import {
 	Row,
 	message,
 } from "antd";
-import "./App.css";
-import "antd/dist/antd.css";
+import "./App.less";
+
 import { useState } from "react";
 import Todo from "./Todo";
 import { PlusOutlined } from "@ant-design/icons";
@@ -63,95 +63,99 @@ function App() {
 		setValue(event.target.value);
 	};
 	return (
-		<Layout style={{ position: "absolute", top: "0%", bottom: "0%", right: "0%", left: "0%" }}>
+		<Layout
+			style={{
+				position: "absolute",
+				top: "0%",
+				bottom: "0%",
+				right: "0%",
+				left: "0%",
+			}}
+		>
 			<Header>
 				<h1 style={{ color: "white" }}>Todo App</h1>
 			</Header>
+			<Content>
+				<Row>
+					<Col span={5}>
+						<MainTasksContainer />
+						<Form onFinish={addMainTask} style={{}}>
+							<Form.Item name="taskName" style={{ margin: "0" }}>
+								<Input
+									style={{ width: "100%", marginTop: "1rem" }}
+									placeholder="Add Task"
+								></Input>
+							</Form.Item>
+							<Form.Item>
+								<Button
+									ghost
+									icon={<PlusOutlined />}
+									style={{
+										display: "inline-block",
+										width: "99%",
+									}}
+									htmlType="submit"
+								>
+									Add Task
+								</Button>
+							</Form.Item>
+						</Form>
+					</Col>
+					<Col span={14}>
+						<Layout>
+							<Header>
+								<Row>
+									<Col span={22}>
+										<h1 style={{ color: "white", fontSize: "large" }}>
+											{selected}
+										</h1>
+									</Col>
+									<Col span={2}></Col>
+								</Row>
+							</Header>
+							<Content style={{ marginTop: "2rem" }}>
+								<div>{todos}</div>
+							</Content>
+						</Layout>
+					</Col>
+					<Col style={{ position: "relative" }} span={5}>
+						<Progress
+							percent={((completed.length / todos.length) * 100).toPrecision(3)}
+							type="circle"
+							width={200}
+							style={{}}
+						/>
+						<Form onFinish={add}>
+							<Form.Item label="Enter Todo" name="content">
+								<Input onChange={onChange} value={value} />
+							</Form.Item>
 
-			<Layout>
-				<Sider>
-					<MainTasksContainer />
-					<Form onFinish={addMainTask} style={{}}>
-						<Form.Item name="taskName" style={{ margin: "0" }}>
-							<Input
-								style={{ width: "100%", marginTop: "1rem" }}
-								placeholder="Add Task"
-							></Input>
-						</Form.Item>
-						<Form.Item>
-							<Button
-								ghost
-								icon={<PlusOutlined />}
-								style={{
-									display: "inline-block",
-									width: "99%",
-								}}
-								htmlType="submit"
-							>
-								Add Task
-							</Button>
-						</Form.Item>
-					</Form>
-				</Sider>
-
-				<Content style={{ overflow: "scroll", scrollbarWidth: "0%" }}>
-					<Layout>
-						<Header>
-							<Row>
-								<Col span={22}>
-									<h1 style={{ color: "white", fontSize: "large" }}>
-										{selected}
-									</h1>
-								</Col>
-								<Col span={2}></Col>
-							</Row>
-						</Header>
-						<Content style={{ marginTop: "2rem" }}>
-							<div>{todos}</div>
-						</Content>
-					</Layout>
-				</Content>
-
-				<Footer style={{ position: "relative" }}>
-					<Progress
-						percent={((completed.length / todos.length) * 100).toPrecision(3)}
-						type="circle"
-						width={200}
-						style={{
-							position: "absolute",
-							top: "70%",
-							left: "50%",
-							transform: "translate(-50%,-50%)",
-						}}
-					/>
-					<Form onFinish={add}>
-						<Form.Item label="Enter Todo" name="content">
-							<Input onChange={onChange} value={value} />
-						</Form.Item>
-
-						<Form.Item name="dueDate" label="Due Date">
-							<DatePicker defaultValue={moment()}></DatePicker>
-						</Form.Item>
-						<Form.Item name="dueTime" label="Due Time">
-							<TimePicker defaultValue={moment("09:00:00", "HH:mm:ss")}></TimePicker>
-						</Form.Item>
-						<Form.Item name="description" label="Description" labelAlign="top">
-							<Input.TextArea rows={5}></Input.TextArea>
-						</Form.Item>
-						<Form.Item>
-							<Button
-								type="primary"
-								htmlType="submit"
-								shape="round"
-								icon={<PlusOutlined />}
-								size="large"
-							>
-								add Todo
-							</Button>
-						</Form.Item>
-					</Form>
-				</Footer>
-			</Layout>
+							<Form.Item name="dueDate" label="Due Date">
+								<DatePicker defaultValue={moment()}></DatePicker>
+							</Form.Item>
+							<Form.Item name="dueTime" label="Due Time">
+								<TimePicker
+									defaultValue={moment("09:00:00", "HH:mm:ss")}
+								></TimePicker>
+							</Form.Item>
+							<Form.Item name="description" label="Description" labelAlign="top">
+								<Input.TextArea rows={5}></Input.TextArea>
+							</Form.Item>
+							<Form.Item>
+								<Button
+									type="primary"
+									htmlType="submit"
+									shape="round"
+									icon={<PlusOutlined />}
+									size="large"
+								>
+									add Todo
+								</Button>
+							</Form.Item>
+						</Form>
+					</Col>
+				</Row>
+			</Content>
 		</Layout>
 	);
 }
